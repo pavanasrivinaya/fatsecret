8d82429a29396b1ec03afaa993709cc4296846f1
Pavana sri vinaya <pavanasri.nyros@gmail.com>
2020-12-14 16:18:46 +0530

    first

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,10 +3,8 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _bbe3ed0a = () => interopDefault(import('..\\pages\\inspire.vue' /* webpackChunkName: "pages/inspire" */))
-const _469bd5be = () => interopDefault(import('..\\pages\\simple.vue' /* webpackChunkName: "pages/simple" */))
+const _018f438d = () => interopDefault(import('..\\pages\\food.vue' /* webpackChunkName: "pages/food" */))
 const _c4952916 = () => interopDefault(import('..\\pages\\todo.vue' /* webpackChunkName: "pages/todo" */))
-const _4507b1b3 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -25,21 +23,13 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
-    path: "/inspire",
-    component: _bbe3ed0a,
-    name: "inspire"
-  }, {
-    path: "/simple",
-    component: _469bd5be,
-    name: "simple"
+    path: "/food",
+    component: _018f438d,
+    name: "food"
   }, {
     path: "/todo",
     component: _c4952916,
     name: "todo"
-  }, {
-    path: "/",
-    component: _4507b1b3,
-    name: "index"
   }],
 
   fallback: false
---
+++ pages/food.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-container style="width: 1800px">
+      <v-app-bar color="deep-purple accent-4" dense>
+        <v-toolbar-title
+          >Search for food, vegetables, fruits - you get to know about
+          nurtitions in it.👁‍🗨
+        </v-toolbar-title>
+      </v-app-bar>
+    </v-container>
+    <v-container
+      id="my_div"
+      class="fatsecret_container"
+      style="background-color: #e9e9e9"
+    >
+    </v-container>
+  </div>
+</template>
